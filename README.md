4868b7aac4711c8bec615c42a7a11a8751b9d4f4
masagrator <masagrator@gmail.com>
2020-02-28 15:06:06 +0100

    First release

---
+++ README.md
@@ -0,0 +1,3 @@
+Games not working with it additionally to SaltyNX excludes
+- The Talos Principle (mix of openGl and Vulkan)
+- The Unholy Society (EGL)
---
+++ source/main.cpp
@@ -0,0 +1,126 @@
+#include <switch_min.h>
+#include "saltysd/SaltySD_ipc.h"
+#include "saltysd/SaltySD_dynamic.h"
+#include "saltysd/SaltySD_core.h"
+
+extern "C" {
+	extern u32 __start__;
+
+	static char g_heap[0x8000];
+
+	void __libnx_init(void* ctx, Handle main_thread, void* saved_lr);
+	void __attribute__((weak)) NORETURN __libnx_exit(int rc);
+	void __nx_exit(int, void*);
+	void __libc_fini_array(void);
+	void __libc_init_array(void);
+	extern u64 nvnBootstrapLoader(const char * nvnName) LINKABLE;
+	extern u64 _ZN2nn2os13GetSystemTickEv() LINKABLE;
+}
+
+u32 __nx_applet_type = AppletType_None;
+Handle orig_main_thread;
+void* orig_ctx;
+void* orig_saved_lr;
+
+void __libnx_init(void* ctx, Handle main_thread, void* saved_lr) {
+	extern char* fake_heap_start;
+	extern char* fake_heap_end;
+
+	fake_heap_start = &g_heap[0];
+	fake_heap_end   = &g_heap[sizeof g_heap];
+	
+	orig_ctx = ctx;
+	orig_main_thread = main_thread;
+	orig_saved_lr = saved_lr;
+	
+	// Call constructors.
+	//void __libc_init_array(void);
+	__libc_init_array();
+}
+
+void __attribute__((weak)) NORETURN __libnx_exit(int rc) {
+	// Call destructors.
+	//void __libc_fini_array(void);
+	__libc_fini_array();
+
+	SaltySD_printf("SaltySD Plugin: jumping to %p\n", orig_saved_lr);
+
+	__nx_exit(0, orig_saved_lr);
+	while (true);
+}
+
+uint8_t FPS = 0xFF;
+float FPSavg = 255;
+uintptr_t ptr_nvnDeviceGetProcAddress;
+uintptr_t ptr_nvnQueuePresentTexture;
+uintptr_t addr_nvnGetProcAddress;
+uintptr_t addr_nvnPresentTexture;
+char FPS_c[8];
+float systemtickfrequency = 19200000;
+typedef void (*nvnQueuePresentTexture_0)(void* unk1_1, void* unk2_1, void* unk3_1);
+typedef uintptr_t (*GetProcAddress)(void* unk1_a, const char * nvnFunction_a);
+
+void nvnPresentTexture(void* unk1, void* unk2, void* unk3) {
+	static uint8_t FPS_temp = 0;
+	static uint64_t starttick = 0;
+	static uint64_t endtick = 0;
+	static uint64_t deltatick = 0;
+	static uint64_t frameend = 0;
+	static uint64_t framedelta = 0;
+	static uint64_t frameavg = 0;
+	
+	if (starttick == 0) starttick = _ZN2nn2os13GetSystemTickEv();
+	((nvnQueuePresentTexture_0)(ptr_nvnQueuePresentTexture))(unk1, unk2, unk3);
+	endtick = _ZN2nn2os13GetSystemTickEv();
+	framedelta = endtick - frameend;
+	frameavg = ((9*frameavg) + framedelta) / 10;
+	FPSavg = systemtickfrequency / (float)frameavg;
+	frameend = endtick;
+	
+	FPS_temp++;
+	deltatick = endtick - starttick;
+	if (deltatick >= 19200000) {
+		starttick = _ZN2nn2os13GetSystemTickEv();
+		FPS = FPS_temp - 1;
+		FPS_temp = 0;
+	}
+	
+	return;
+}
+
+uintptr_t nvnGetProcAddress (void* unk1, const char* nvnFunction) {
+	uintptr_t address = ((GetProcAddress)(ptr_nvnDeviceGetProcAddress))(unk1, nvnFunction);
+	if (strcmp("nvnDeviceGetProcAddress", nvnFunction) == 0) return addr_nvnGetProcAddress;
+	else if (strcmp("nvnQueuePresentTexture", nvnFunction) == 0) {
+		ptr_nvnQueuePresentTexture = address;
+		return addr_nvnPresentTexture;
+	}
+	else return address;
+}
+
+uintptr_t nvnBootstrapLoader_1(const char* nvnName) {
+	if (strcmp(nvnName, "nvnDeviceGetProcAddress") == 0) {
+		ptr_nvnDeviceGetProcAddress = nvnBootstrapLoader("nvnDeviceGetProcAddress");
+		return addr_nvnGetProcAddress;
+	}
+	uintptr_t ptrret = nvnBootstrapLoader(nvnName);
+	return ptrret;
+}
+
+int main(int argc, char *argv[]) {
+	uint64_t addr_FPS;
+	uint64_t addr_avg_FPS;
+	SaltySD_printf("FPS Counter: alive\n");
+	addr_nvnGetProcAddress = &nvnGetProcAddress;
+	addr_nvnPresentTexture = &nvnPresentTexture;
+	addr_FPS = &FPS;
+	FILE* offset = SaltySDCore_fopen("sdmc:/SaltySD/FPSoffset.hex", "wb");
+	SaltySDCore_fwrite(&addr_FPS, 0x5, 1, offset);
+	SaltySDCore_fclose(offset);
+	addr_avg_FPS = &FPSavg;
+	FILE* avgoffset = SaltySDCore_fopen("sdmc:/SaltySD/FPSavgoffset.hex", "wb");
+	SaltySDCore_fwrite(&addr_avg_FPS, 0x5, 1, avgoffset);
+	SaltySDCore_fclose(avgoffset);
+	SaltySDCore_ReplaceImport("nvnBootstrapLoader", &nvnBootstrapLoader_1);
+	SaltySD_printf("FPS Counter: injection finished\n");
+}
