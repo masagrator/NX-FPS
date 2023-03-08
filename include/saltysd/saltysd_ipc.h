5d0018cf4dfa4553600749c1ee8c98dc4306ca31
MasaGratoR <masagrator@gmail.com>
2023-03-08 17:46:45 +0100

    Update saltysd_ipc.h

--- include/saltysd/saltysd_ipc.h
+++ include/saltysd/saltysd_ipc.h
@@ -15,6 +15,7 @@ extern "C" {
 	Result SaltySD_GetSDCard(Handle *retrieve) LINKABLE;
 	Result SaltySD_CheckIfSharedMemoryAvailable(ptrdiff_t *offset, u64 size) LINKABLE;
 	Result SaltySD_GetSharedMemoryHandle(Handle *retrieve) LINKABLE;
+	Result SaltySD_GetBID(u8* BID) LINKABLE;
 	Result SaltySD_printf(const char* format, ...) LINKABLE;
 }
 
